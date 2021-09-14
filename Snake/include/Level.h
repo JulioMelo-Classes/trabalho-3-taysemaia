ba5d67603393c6c0c59b47e244337a1ac10e7b8c
Tayse <53482565+taysemaia@users.noreply.github.com>
2021-09-14 00:25:48 -0300

    Update Level.h

--- Snake/include/Level.h
+++ Snake/include/Level.h
@@ -18,24 +18,36 @@ class Level{
 
 	public:
 	 	/*! @brief construtor 
-            @param alt a quantidade de linhas do lvl
-			@param larg a quantidade de colunas do lvl
-			@param com a quantidade de comidas do lvl
-         */
+            	@param alt a quantidade de linhas do lvl
+	   	@param larg a quantidade de colunas do lvl
+		@param com a quantidade de comidas do lvl
+        	*/
 		Level(int alt, int larg, int com, std::vector<std::string> maze);
 
 		/*! @brief Funçao para imprimir o labirinto atual
-         */
+        	 */
 		void mostrarLabirinto();
 
 		/*! @brief Funçao para imprimir a interface com informaçoes do level
-         */
+         	*/
 		void mostrarInterface();
-		/*! Funçao para gerar a cobra em uma posição aleatória
-         */
+	
+		/*! Funçao para gerar uma posição aleatória válida
+		@return um par com a posiçao gerada
+         	*/
 		std::pair<int, int> randomStart();
+	
+		/*! Funçao para adicionar a cobra na posiçao aleatoria
+         	*/
 		void adicionaCobra();
+	
+		/*! Funçao para criar uma posiçao aleatoria para a comida
+		@return um par com a posiçao gerada
+         	*/
 		std::pair<int, int> foodRandomStart();
+		
+		/*! Funçao para adicionar a comida na posiçao aleatoria
+         	*/
 		void adicionaComida();
 
 };
